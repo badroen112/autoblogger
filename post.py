e0adcc93ccadb60a9ff76032e5835f87a62966eb
badroen112 <59379670+badroen112@users.noreply.github.com>
2026-08-05 07:48:56 +0700

    Update post.py

--- post.py
+++ post.py
@@ -8,3 +8,34 @@ entry = feed.entries[0]
 
 print(entry.title)
 print(entry.link)
+
+with open("last_post.txt") as f:
+    last = f.read().strip()
+
+latest = feed.entries[0].link
+
+if latest == last:
+    print("Tidak ada artikel baru.")
+    exit()
+
+tweet = f"""
+{entry.title}
+
+{entry.link}
+
+#AI #Blogger
+"""
+
+import tweepy
+
+client = tweepy.Client(
+    consumer_key=...,
+    consumer_secret=...,
+    access_token=...,
+    access_token_secret=...
+)
+
+client.create_tweet(text=tweet)
+
+with open("last_post.txt","w") as f:
+    f.write(latest)
